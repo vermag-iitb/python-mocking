818b35f4c2fa1d0fadbfbb5ad096205da47cfdcd
vermag-iitb <gaurav72verma@yahoo.in>
2023-01-20 00:58:37 +0530

    Added get_joke()

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 import requests
+def len_joke():
+    joke = get_joke()
+    return len(joke)
+
+def get_joke():
+    url = 'http://api.icndb.com/jokes/random'
+    response = requests.get(url)
+    if response.status_code == 200:
+        joke = response.json()['value']['joke']
+    else:
+        joke = 'No jokes'
+    return joke
+
+if __name__ == '__main__':
+    print(get_joke())
